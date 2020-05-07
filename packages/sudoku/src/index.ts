b29c8bf1cb889d4b3a110c19cf7192cd56c9de8c
donmahallem <donmahallem@users.noreply.github.com>
2020-05-07 07:51:11 +0200

    fix(sudoku): remove unused class left from testing

--- packages/sudoku/src/index.ts
+++ packages/sudoku/src/index.ts
@@ -2,10 +2,5 @@
  * Source https://github.com/spielhalle/spielhalle Package: @spielhalle/sudoku
  */
 
-export class TestClass {
-    public cast(): void {
-        console.log('HELLO');
-    }
-}
 export * from './backtrack';
 export * from './coverboard';
