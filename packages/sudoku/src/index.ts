d9e405661608ab325434885d4cdc0e6120d636cf
DonMahallem <donmahallem@users.noreply.github.com>
2021-02-22 19:47:31 +0100

    chore: remove @spielhalle/sudoku package

--- packages/sudoku/src/index.ts
+++
@@ -1,8 +0,0 @@
-/**!
- * Source https://github.com/spielhalle/spielhalle Package: @spielhalle/sudoku
- */
-
-export * from './backtrack';
-export * from './coverboard';
-export * from './create-empty-sudoku-board';
-export * from './create-sudoku-board';
