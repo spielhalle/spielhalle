a8e41cd6241c01f9aa5039b5abc7caf04ed2e449
DonMahallem <donmahallem@users.noreply.github.com>
2021-03-24 13:00:07 +0100

    style(tank-call): fix style issues in landscape

--- packages/tank-call/src/landscape.ts
+++ packages/tank-call/src/landscape.ts
@@ -1,39 +1,90 @@
-import { Graphics } from "@pixi/graphics";
+/*!
+ * Source https://github.com/spielhalle/spielhalle Package: tank-call
+ */
 
-type Point = {
-    x: number;
-    y: number;
-}
-
+import { Graphics } from '@pixi/graphics';
+import { IPointData } from '@pixi/math';
 
 export class Landscape extends Graphics {
     private data: number[] = [];
     private sourceData: number[];
-    private _hillHeight: number;
-    private _hillOffset: number;
-    private _hillWidth: number;
-    private _deltaHeight: number;
-    private _octaves: number;
+    private mHillHeight: number;
+    private mHillOffset: number;
+    private mHillWidth: number;
+    private mOctaves: number;
+    private static bezier(p0: IPointData, p1: IPointData, p2: IPointData, p3: IPointData, t: number): IPointData {
+        return {
+            x: (Math.pow(1 - t, 3) * p0.x) +
+                (3 * t * Math.pow(1 - t, 2) * p1.x) +
+                (3 * Math.pow(t, 2) * (1 - t) * p2.x) +
+                (Math.pow(t, 3) * p3.x),
+            y: (Math.pow(1 - t, 3) * p0.y) +
+                (3 * t * Math.pow(1 - t, 2) * p1.y) +
+                (3 * Math.pow(t, 2) * (1 - t) * p2.y) +
+                (Math.pow(t, 3) * p3.y),
+        };
+    }
+    private static generate(width: number, ocataves: number = 6): number[] {
+        if (width % 1 !== 0) {
+            throw new Error('Only integers as width allowed');
+        }
+        if (ocataves % 1 !== 0) {
+            throw new Error('Only integers as octaves allowed');
+        }
+        const res: number[] = [];
+        const stepWidth: number = Math.ceil(width / ocataves);
+        for (let i: number = 0; i <= ocataves; i++) {
+            res[i * stepWidth] = Math.random();
+            if (i > 0) {
+                Landscape.fillBezier(res, (i - 1) * stepWidth, i * stepWidth);
+            }
+        }
+        return res;
+    }
+
+    private static fillBezier(data: number[], idxL: number, idxR: number): void {
+        const dX: number = idxR - idxL;
+        const idxC: number = (idxR + idxL) / 2;
+        const p0: IPointData = {
+            x: idxL,
+            y: data[idxL],
+        };
+        const p3: IPointData = {
+            x: idxR,
+            y: data[idxR],
+        };
+        const p1: IPointData = {
+            x: idxC,
+            y: data[idxL],
+        };
+        const p2: IPointData = {
+            x: idxC,
+            y: data[idxR],
+        };
+        for (let i: number = 0; i < dX; i++) {
+            data[idxL + i] = Landscape.bezier(p0, p1, p2, p3, i / dX).y;
+        }
+    }
     public constructor(hillWidth: number, hillHeight: number, hillOffset: number, octaves: number) {
         super();
         if (hillHeight <= 0) {
-            throw new Error("hillHeight must larger than 0");
+            throw new Error('hillHeight must larger than 0');
         }
         if (hillWidth <= 0) {
-            throw new Error("hillWidth must larger than 0");
+            throw new Error('hillWidth must larger than 0');
         }
-        this._hillHeight = hillHeight;
-        this._hillOffset = hillOffset;
-        this._hillWidth = hillWidth;
-        this._octaves = octaves;
+        this.mHillHeight = hillHeight;
+        this.mHillOffset = hillOffset;
+        this.mHillWidth = hillWidth;
+        this.mOctaves = octaves;
         this.regenerate();
     }
 
-    public explodeAt(_x: number, radius: number = 10) {
-        let x = Math.round(_x);
-        let startHeight: number = this.getHeight(x);
-        for (let i = x - radius; i <= radius + x; i++) {
-            let dX = i - x;
+    public explodeAt(x: number, radius: number = 10): void {
+        const roundedX: number = Math.round(x);
+        const startHeight: number = this.getHeight(roundedX);
+        for (let i: number = roundedX - radius; i <= radius + roundedX; i++) {
+            const dX: number = i - roundedX;
             this.data[i] = Math.min(this.data[i], startHeight - Math.sin(Math.acos(dX / radius)) * radius);
             if (this.data[i] <= 0) {
                 this.data[i] = 1;
@@ -47,44 +98,44 @@ export class Landscape extends Graphics {
     }
 
     public set hillHeight(height: number) {
-        this._hillHeight = height;
+        this.mHillHeight = height;
         this.redraw();
     }
 
     public get hillHeight(): number {
-        return this._hillHeight;
+        return this.mHillHeight;
     }
 
     public set hillWidth(width: number) {
-        this._hillWidth = width;
+        this.mHillWidth = width;
         this.regenerate();
     }
 
     public get hillWidth(): number {
-        return this._hillWidth;
+        return this.mHillWidth;
     }
 
     public set hillOffset(offset: number) {
-        this._hillOffset = offset;
+        this.mHillOffset = offset;
         this.redraw();
     }
 
     public get hillOffset(): number {
-        return this._hillOffset;
+        return this.mHillOffset;
     }
     public set octaves(octaves: number) {
-        this._octaves = octaves;
+        this.mOctaves = octaves;
         this.regenerate();
     }
 
     public get octaves(): number {
-        return this._octaves;
+        return this.mOctaves;
     }
 
     public regenerate(): void {
-        this.sourceData = Landscape.generate(this._hillWidth, this._octaves);
-        for (let x = 0; x < this.sourceData.length; x++) {
-            this.data[x] = this._hillOffset + (this.sourceData[x] * this._hillHeight);
+        this.sourceData = Landscape.generate(this.mHillWidth, this.mOctaves);
+        for (let x: number = 0; x < this.sourceData.length; x++) {
+            this.data[x] = this.mHillOffset + (this.sourceData[x] * this.mHillHeight);
         }
         this.redraw();
     }
@@ -102,55 +153,5 @@ export class Landscape extends Graphics {
         this.lineTo(this.width, 0);
         this.lineTo(0, 0);
         this.endFill();
-    }
-
-    private static generate(width: number, ocataves: number = 6): number[] {
-        if (width % 1 != 0) {
-            throw new Error("Only integers as width allowed");
-        }
-        if (ocataves % 1 != 0) {
-            throw new Error("Only integers as octaves allowed");
-        }
-        let res: number[] = [];
-        let w = width;
-        let stepWidth: number = Math.ceil(width / ocataves);
-        for (let i = 0; i <= ocataves; i++) {
-            res[i * stepWidth] = Math.random();
-            if (i > 0) {
-                this.fillBezier(res, (i - 1) * stepWidth, i * stepWidth);
-            }
-        }
-        return res;
-    }
-
-    private static fillBezier(data: number[], idxL: number, idxR: number) {
-        let dX = idxR - idxL;
-        let idxC = (idxR + idxL) / 2;
-        let p0: Point = {
-            x: idxL,
-            y: data[idxL]
-        }
-        let p3: Point = {
-            x: idxR,
-            y: data[idxR]
-        }
-        let p1: Point = {
-            x: idxC,
-            y: data[idxL]
-        }
-        let p2: Point = {
-            x: idxC,
-            y: data[idxR]
-        }
-        for (let i = 0; i < dX; i++) {
-            data[idxL + i] = this.bezier(p0, p1, p2, p3, i / dX).y
-        }
-    }
-
-    private static bezier(p0: Point, p1: Point, p2: Point, p3: Point, t: number): Point {
-        return {
-            x: (Math.pow(1 - t, 3) * p0.x) + (3 * t * Math.pow(1 - t, 2) * p1.x) + (3 * Math.pow(t, 2) * (1 - t) * p2.x) + (Math.pow(t, 3) * p3.x),
-            y: (Math.pow(1 - t, 3) * p0.y) + (3 * t * Math.pow(1 - t, 2) * p1.y) + (3 * Math.pow(t, 2) * (1 - t) * p2.y) + (Math.pow(t, 3) * p3.y)
-        };
     }
 }
