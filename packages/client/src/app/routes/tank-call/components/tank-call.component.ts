9f2e7c85a40df826f2fd9056a1e0c8b7b92893a8
DonMahallem <donmahallem@users.noreply.github.com>
2021-06-14 11:30:34 +0200

    style(client): fix lint issues

--- packages/client/src/app/routes/tank-call/components/tank-call.component.ts
+++ packages/client/src/app/routes/tank-call/components/tank-call.component.ts
@@ -30,7 +30,6 @@ export class TankCallComponent implements AfterViewInit, OnDestroy {
     constructor(public elRef: ElementRef,
         private zone: NgZone) {
 
-
         // The application will create a canvas element for you that you
         // can then insert into the DOM.
 
@@ -38,60 +37,49 @@ export class TankCallComponent implements AfterViewInit, OnDestroy {
 
     public ngAfterViewInit(): void {
         this.zone.runOutsideAngular((): void => {
-            const ticker: Ticker = Ticker.shared
+            const ticker: Ticker = Ticker.shared;
             ticker.autoStart = false;
             ticker.stop();
             this.renderer = autoDetectRenderer({
-                backgroundAlpha: 0.5,
-                view: this.myCanvas.nativeElement,
                 antialias: true,    // default: false
+                backgroundAlpha: 0.5,
                 // transparent: environment.production, // default: false
-                resolution: 1       // default: 1,
-                ,
+                resolution: 1,       // default: 1,
+                view: this.myCanvas.nativeElement,
             });
             this.tankGame = new TankGame(256, 256);
             const stage: Container = new Container();
             stage.addChild(this.tankGame);
             stage.pivot.y = this.renderer.height;
             stage.scale.y = -1;
-            //as.height = 100;
-            //as.width = 100;
-            //as.x = 50;
-            //as.y = 40;
-            //app.render();
             const loader: Loader = new Loader('https://avatars.githubusercontent.com/u/');
-            //This `setup` function will run when the image has loaded
-            const setup = () => {
-
-                //Create the cat sprite
-                let cat = new Sprite(loader.resources["cat"].texture);
-
-                //Add the cat to the stage
+            const setup: () => void = (): void => {
+                const cat: Sprite = new Sprite(loader.resources.cat.texture);
                 stage.addChild(cat);
-            }
+            };
             loader
-                .add("cat", "695831?s=88&u=6114c2fc6a6b89449c4eb0d13a65b29adfad0e4d&v=4")
+                .add('cat', '695831?s=88&u=6114c2fc6a6b89449c4eb0d13a65b29adfad0e4d&v=4')
                 .load(setup);
             stage.render(this.renderer as any);
 
             const fna = (): void => {
                 requestAnimationFrame(fna);
                 this.renderer.render(stage);
-            }
+            };
             requestAnimationFrame(fna);
 
-        })
+        });
 
-        this.resizeObservable$ = fromEvent(window, 'resize')
-        this.resizeSubscription$ = this.resizeObservable$.subscribe(evt => {
-            console.log('event: ', evt)
+        this.resizeObservable$ = fromEvent(window, 'resize');
+        this.resizeSubscription$ = this.resizeObservable$.subscribe((evt: any): void => {
+            console.log('event: ', evt);
             if (this.renderer) {
-                //this.tankGame.scale.resize(this.myCanvas.nativeElement.width, this.myCanvas.nativeElement.height);
+                // this.tankGame.scale.resize(this.myCanvas.nativeElement.width, this.myCanvas.nativeElement.height);
             }
-        })
+        });
     }
-    ngOnDestroy() {
-        this.resizeSubscription$.unsubscribe()
+    public ngOnDestroy(): void {
+        this.resizeSubscription$.unsubscribe();
     }
 
     public onFieldChange(sudokuField: number[][]): void {
