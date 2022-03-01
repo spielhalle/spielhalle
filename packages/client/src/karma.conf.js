ac8bbe9bc3e8e54a7ed183839ba82aee1f5ce0af
DonMahallem <donmahallem@users.noreply.github.com>
2022-03-01 15:33:01 +0100

    fix(client): fix coverage report creation

--- packages/client/src/karma.conf.js
+++ packages/client/src/karma.conf.js
@@ -10,13 +10,27 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/spielhalle'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' },
+        { type: 'lcovonly' },
+        { type: 'json' }
+      ]
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
+    },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/spielhalle'),
+      dir: require('path').join(__dirname, './coverage/spielhalle2'),
       reports: ['html', 'lcovonly', 'text-summary', 'json'],
       fixWebpackSourcePaths: true
     },
