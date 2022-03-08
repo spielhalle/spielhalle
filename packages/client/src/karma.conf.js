f81fcf5b9695ae799c3317248019c884555d0e72
DonMahallem <donmahallem@users.noreply.github.com>
2022-03-08 14:51:29 +0100

    test(client): fix test coverage generation

--- packages/client/src/karma.conf.js
+++ packages/client/src/karma.conf.js
@@ -10,15 +10,24 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/spielhalle'),
-      reports: ['html', 'lcovonly', 'text-summary', 'json'],
-      fixWebpackSourcePaths: true
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/spielhalle'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' },
+        { type: 'lcovonly' },
+        { type: 'json' }
+      ]
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
